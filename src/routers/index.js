f3c703d71332107b9ee401b2af7293ea0dd6269e
leex <jsonleex@163.com>
2018-04-12 19:33:35 +0800

    Update index.js

--- src/routers/index.js
+++ src/routers/index.js
@@ -8,7 +8,9 @@ Vue.use(VueRouter);
 
 const router = new VueRouter({
   routes,
-  mode: "hash",
+  base: process.env.BASE_URL,
+  strict: process.env.NODE_ENV !== "production",
+  mode: process.env.VUE_APP_ROUTER_MODE || "hash",
   scrollBehavior(to, from, savedPosition) {
     if (to.hash) {
       return {
