330f6c3241f709eeda28812157436f9a1ad4133d
Seven Du <shiweidu@outlook.com>
2018-07-09 19:21:00 +0800

    chore: Build SPA & Update assets.

---
+++ precache-manifest.d917797d57dbffe066d91ecad72dbbbb.js
@@ -0,0 +1,126 @@
+self.__precacheManifest = [
+  {
+    "revision": "e46bc22bb5432925022c",
+    "url": "/plus-small-screen-client/css/app.38c2e1b6.css"
+  },
+  {
+    "revision": "e3115184c21ceef3f6b26d809cea732b",
+    "url": "/plus-small-screen-client/libs/strophe-1.2.8.min.js"
+  },
+  {
+    "revision": "e46bc22bb5432925022c",
+    "url": "/plus-small-screen-client/js/app.e46bc22b.js"
+  },
+  {
+    "revision": "bd856a76531965ec67fd",
+    "url": "/plus-small-screen-client/css/question.e41fcd63.css"
+  },
+  {
+    "revision": "53ac1c0967be90b02903f492cb85ce41",
+    "url": "/plus-small-screen-client/index.html"
+  },
+  {
+    "url": "/plus-small-screen-client/js/post-326d787da5c433a00931.js"
+  },
+  {
+    "revision": "c0714d129de43d4163ca",
+    "url": "/plus-small-screen-client/css/rank.3ab89a53.css"
+  },
+  {
+    "url": "/plus-small-screen-client/js/rank-c0714d129de43d4163ca.js"
+  },
+  {
+    "revision": "355b1b857f890ed55d304c77603eebfb",
+    "url": "/plus-small-screen-client/img/ico_question@3x.355b1b85.png"
+  },
+  {
+    "url": "/plus-small-screen-client/js/news-f2a2a55b20492e9fbd79.js"
+  },
+  {
+    "revision": "27b2a1351d91f3d80051cbf6a00f0e1f",
+    "url": "/plus-small-screen-client/img/ico_word@3x.27b2a135.png"
+  },
+  {
+    "url": "/plus-small-screen-client/js/feed-0a2973ae1c042be65e6d.js"
+  },
+  {
+    "revision": "32e6c0438a69ced377b2",
+    "url": "/plus-small-screen-client/css/wallet.43cb182a.css"
+  },
+  {
+    "url": "/plus-small-screen-client/js/wallet-32e6c0438a69ced377b2.js"
+  },
+  {
+    "revision": "89c0f60004bc4f3e4b76a77b0e1a2d44",
+    "url": "/plus-small-screen-client/img/ico_attendance@3x.89c0f600.png"
+  },
+  {
+    "url": "/plus-small-screen-client/js/chunk-vendors-c7eceba1b48005d972bc.js"
+  },
+  {
+    "url": "/plus-small-screen-client/js/question-bd856a76531965ec67fd.js"
+  },
+  {
+    "url": "/plus-small-screen-client/js/message-8a86e46e299832d778cb.js"
+  },
+  {
+    "revision": "1ac08eca22770cf6d64f93207de53081",
+    "url": "/plus-small-screen-client/img/pic_default_secret.1ac08eca.png"
+  },
+  {
+    "revision": "0bcff4e589e5cc244f83163513570485",
+    "url": "/plus-small-screen-client/img/img_default_search@2x.0bcff4e5.png"
+  },
+  {
+    "revision": "8dc4fad355a26aadcfc6b7b6710cffac",
+    "url": "/plus-small-screen-client/img/pic_default_woman.8dc4fad3.png"
+  },
+  {
+    "revision": "1d7e5008cb945f72b5c2cd3b9d030793",
+    "url": "/plus-small-screen-client/img/ico_ts_assistant@2x.1d7e5008.png"
+  },
+  {
+    "revision": "1b80cde03faf06ffd08b8a64e8f707ff",
+    "url": "/plus-small-screen-client/img/pic_default_man.1b80cde0.png"
+  },
+  {
+    "revision": "df39055cc5065a8ef03531aa56fe8c3f",
+    "url": "/plus-small-screen-client/img/logo_thinksns+@2x.df39055c.png"
+  },
+  {
+    "revision": "b257fa9c5ac8c515ac4d77a667ce2943",
+    "url": "/plus-small-screen-client/img/default-skin.b257fa9c.svg"
+  },
+  {
+    "revision": "a9c07f4ac7de1fad803f4755de893092",
+    "url": "/plus-small-screen-client/img/user_home_default_cover.a9c07f4a.png"
+  },
+  {
+    "revision": "db4963b84a426aa8f0165f4f4fc119c7",
+    "url": "/plus-small-screen-client/img/ico_contribute@3x.db4963b8.png"
+  },
+  {
+    "revision": "0c37c6e2f5c50e12acc1a59cdc0e95f5",
+    "url": "/plus-small-screen-client/img/ico_potoablum@3x.0c37c6e2.png"
+  },
+  {
+    "revision": "326d787da5c433a00931",
+    "url": "/plus-small-screen-client/css/post.06c64c94.css"
+  },
+  {
+    "revision": "f2a2a55b20492e9fbd79",
+    "url": "/plus-small-screen-client/css/news.fd98a1e6.css"
+  },
+  {
+    "revision": "8a86e46e299832d778cb",
+    "url": "/plus-small-screen-client/css/message.edb4e141.css"
+  },
+  {
+    "revision": "0a2973ae1c042be65e6d",
+    "url": "/plus-small-screen-client/css/feed.356494b3.css"
+  },
+  {
+    "revision": "c7eceba1b48005d972bc",
+    "url": "/plus-small-screen-client/css/chunk-vendors.c1533543.css"
+  }
+];
