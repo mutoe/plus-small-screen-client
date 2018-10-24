bf4827df3a14440c7771481e33a73f58a5a3135c
mutoe <mutoe@foxmail.com>
2018-10-24 17:57:12 +0800

    fix(find): (#576) 推荐列表没有获取与用户相关标签的推荐

--- src/page/find/FindRec.vue
+++ src/page/find/FindRec.vue
@@ -7,17 +7,17 @@
     <user-item
       v-for="user in users"
       :user="user"
-      :key="user.id"/>
+      :key="`${user.id}-from-${user.searchFrom}`"/>
   </jo-load-more>
 </template>
+
 <script>
 import UserItem from "@/components/UserItem.vue";
-import { findUserByType } from "@/api/user.js";
+import * as userApi from "@/api/user";
+
 export default {
-  name: "FindPop",
-  components: {
-    UserItem
-  },
+  name: "FindRec",
+  components: { UserItem },
   data() {
     return {
       users: []
@@ -27,19 +27,42 @@ export default {
     this.$refs.loadmore.beforeRefresh();
   },
   methods: {
-    onRefresh(callback) {
-      findUserByType("recommends").then(({ data: users } = {}) => {
-        users && (this.users = users);
-        callback(users.length < 15);
-      });
+    onRefresh() {
+      const recommendPromise = userApi
+        .findUserByType("recommends")
+        .then(({ data: users }) => {
+          this.users = users;
+          return users.map(u => {
+            u.searchFrom = "recommend";
+            return u;
+          });
+        });
+      const tagsPromise = userApi
+        .findUserByType("find-by-tags")
+        .then(({ data: users }) => {
+          this.users = users;
+          this.$refs.loadmore.afterRefresh(users.length < 15);
+          return users.map(u => {
+            u.searchFrom = "tags";
+            return u;
+          });
+        });
+      // 并发获取用户
+      Promise.all([recommendPromise, tagsPromise])
+        .then(([recommendUsers, tagsUsers]) => {
+          this.users = [...recommendUsers, ...tagsUsers];
+        })
+        .catch(err => {
+          this.$refs.loadmore.afterRefresh(false);
+          return err;
+        });
     },
-    onLoadMore(callback) {
-      findUserByType("recommends", {
+    async onLoadMore() {
+      const { data: users } = await userApi.findUserByType("find-by-tags", {
         offset: this.users.length
-      }).then(({ data: users }) => {
-        this.users = [...this.users, ...users];
-        callback(users.length < 15);
       });
+      this.users = [...this.users, ...users];
+      this.$refs.loadmore.afterLoadMore(users.length < 15);
     }
   }
 };
