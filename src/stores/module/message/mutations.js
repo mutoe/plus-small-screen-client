d35e116b22ae817170e97b9db9ace728e47af32e
mutoe <mutoe@foxmail.com>
2018-10-25 17:07:19 +0800

    refactor: (#596) 移除消息列表审核模块

--- src/stores/module/message/mutations.js
+++ src/stores/module/message/mutations.js
@@ -14,6 +14,26 @@ export default {
     };
   },
   /**
+   * 保存动态评论审核
+   * @Author   Wayne
+   * @DateTime 2018-02-06
+   * @Email    qiaobin@zhiyicx.com
+   * @param    {[type]}            state   [description]
+   * @param    {[type]}            options [description]
+   */
+  SAVE_FEED_COMMENT_AUDITS(state, options) {
+    if (options.type === "all") {
+      state.MY_COMMENT_AUDIT = options.data;
+    } else if (options.type === "new") {
+      state.MY_COMMENT_AUDIT = _.unionBy(
+        [...options.data, ...state.MY_COMMENT_AUDIT],
+        "id"
+      );
+    } else {
+      state.MY_COMMENT_AUDIT = [...state.MY_COMMENT_AUDIT, ...options.data];
+    }
+  },
+  /**
    * 保存我收到的评论
    * @Author   Wayne
    * @DateTime 2018-01-29
@@ -49,118 +69,6 @@ export default {
       state.MY_LIKED = _.unionBy([...options.data, ...state.MY_LIKED], "id");
     } else {
       state.MY_LIKED = [...state.MY_LIKED, ...options.data];
-    }
-  },
-
-  /**
-   * 保存动态评论审核
-   * @Author   Wayne
-   * @DateTime 2018-02-06
-   * @Email    qiaobin@zhiyicx.com
-   * @param    {[type]}            state   [description]
-   * @param    {[type]}            options [description]
-   */
-  SAVE_FEED_COMMENT_AUDITS(state, options) {
-    if (options.type === "all") {
-      state.MY_COMMENT_AUDIT = options.data;
-    } else if (options.type === "new") {
-      state.MY_COMMENT_AUDIT = _.unionBy(
-        [...options.data, ...state.MY_COMMENT_AUDIT],
-        "id"
-      );
-    } else {
-      state.MY_COMMENT_AUDIT = [...state.MY_COMMENT_AUDIT, ...options.data];
-    }
-  },
-
-  /**
-   * 保存资讯评论审核
-   * @Author   Wayne
-   * @DateTime 2018-02-06
-   * @Email    qiaobin@zhiyicx.com
-   * @param    {[type]}            state   [description]
-   * @param    {[type]}            options [description]
-   */
-  SAVE_NEWS_COMMENT_AUDITS(state, options) {
-    if (options.type === "all") {
-      state.MY_NEWS_COMMENT_AUDIT = options.data;
-    } else if (options.type === "new") {
-      state.MY_NEWS_COMMENT_AUDIT = _.unionBy(
-        [...options.data, ...state.MY_NEWS_COMMENT_AUDIT],
-        "id"
-      );
-    } else {
-      state.MY_NEWS_COMMENT_AUDIT = [
-        ...state.MY_NEWS_COMMENT_AUDIT,
-        ...options.data
-      ];
-    }
-  },
-
-  /**
-   * 保存加入圈子审核
-   * @Author   Wayne
-   * @DateTime 2018-02-06
-   * @Email    qiaobin@zhiyicx.com
-   * @param    {[type]}            state   [description]
-   * @param    {[type]}            options [description]
-   */
-
-  SAVE_JOIN_GROUP_AUDITS(state, options) {
-    if (options.type === "all") {
-      state.MY_GROUP_JOIN_AUDIT = options.data;
-    } else if (options.type === "new") {
-      state.MY_GROUP_JOIN_AUDIT = _.unionBy(
-        [...options.data, ...state.MY_GROUP_JOIN_AUDIT],
-        "id"
-      );
-    } else {
-      state.MY_POST_AUDIT = [...state.MY_GROUP_JOIN_AUDIT, ...options.data];
-    }
-  },
-
-  /**
-   * 保存帖子置顶审核
-   * @Author   Wayne
-   * @DateTime 2018-02-06
-   * @Email    qiaobin@zhiyicx.com
-   * @param    {[type]}            state   [description]
-   * @param    {[type]}            options [description]
-   */
-  SAVE_GROUP_POST_AUDITS(state, options) {
-    if (options.type === "all") {
-      state.MY_POST_AUDIT = options.data;
-    } else if (options.type === "new") {
-      state.MY_POST_AUDIT = _.unionBy(
-        [...options.data, ...state.MY_POST_AUDIT],
-        "id"
-      );
-    } else {
-      state.MY_POST_AUDIT = [...state.MY_POST_AUDIT, ...options.data];
-    }
-  },
-
-  /**
-   * 保存帖子评论审核
-   * @Author   Wayne
-   * @DateTime 2018-02-06
-   * @Email    qiaobin@zhiyicx.com
-   * @param    {[type]}            state   [description]
-   * @param    {[type]}            options [description]
-   */
-  SAVE_POST_COMMENT_AUDITS(state, options) {
-    if (options.type === "all") {
-      state.MY_POST_COMMENT_AUDIT = options.data;
-    } else if (options.type === "new") {
-      state.MY_POST_COMMENT_AUDIT = _.unionBy(
-        [...options.data, ...state.MY_POST_COMMENT_AUDIT],
-        "id"
-      );
-    } else {
-      state.MY_POST_COMMENT_AUDIT = [
-        ...state.MY_POST_COMMENT_AUDIT,
-        ...options.data
-      ];
     }
   }
 };
