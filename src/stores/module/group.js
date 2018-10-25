d526f539b30ff4343826d15033ab23585d26e28c
mutoe <mutoe@foxmail.com>
2018-10-25 16:36:52 +0800

    refactor: (#596) 移除圈子模块

--- src/stores/module/group.js
+++ src/stores/module/group.js
@@ -1,6 +1,32 @@
 import * as api from "@/api/group";
+import lstore from "@/plugins/lstore/lstore";
 
-export default {
+export const TYPES = {
+  SAVE_GROUP: "SAVE_GROUP",
+  SAVE_GROUP_CATES: "SAVE_GROUP_CATES"
+};
+
+const state = {
+  categories: lstore.getData("GROUP_CATES") || [], // 圈子分类
+  current: {}, // 当前查看的圈子
+  protocol: ""
+};
+
+const getters = {};
+
+const mutations = {
+  [TYPES.SAVE_GROUP](state, group) {
+    state.current = group;
+  },
+
+  // 保存圈子分类列表
+  [TYPES.SAVE_GROUP_CATES](state, cates) {
+    state.categories = cates;
+    lstore.setData("GROUP_CATES", cates);
+  }
+};
+
+const actions = {
   /**
    * 获取我加入的圈子
    * @author mutoe <mutoe@foxmail.com>
@@ -28,4 +54,12 @@ export default {
       : [];
     return data;
   }
+};
+
+export default {
+  namespaced: true,
+  state,
+  getters,
+  mutations,
+  actions
 };
