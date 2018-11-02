29930e2be67e653aa9712b7bf7127ed43d2c8073
mutoe <mutoe@foxmail.com>
2018-11-02 15:58:22 +0800

    fix(question): 问题时间显示为创建时间而非更新时间

--- src/page/question/components/QuestionCard.vue
+++ src/page/question/components/QuestionCard.vue
@@ -34,7 +34,7 @@
       <span v-show="question.amount" class="shang">
         <span>赏</span> {{ question.amount }}
       </span>
-      <span class="button-time">{{ question.updated_at | time2tips }}</span>
+      <span class="button-time">{{ question.created_at | time2tips }}</span>
     </router-link>
   </div>
 </template>
